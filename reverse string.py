c4100ca7af43ded81556f1f94c777a07735848b3
Kevin Lin <kevinlin47@hotmail.com>
2017-08-31 21:22:37 -0400

    Delete reverse string.py

--- reverse string.py
+++
@@ -1,9 +0,0 @@
-Python 3.6.1rc1 (v3.6.1rc1^0:e0fbe5feee4f9c00f09eb9659c2182183036261a, Mar  4 2017, 20:00:12) [MSC v.1900 64 bit (AMD64)] on win32
-Type "copyright", "credits" or "license()" for more information.
->>> string=input()
-flip me
->>> print(string)
-flip me
->>> print(string[::-1])
-em pilf
->>> 
