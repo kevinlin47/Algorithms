2e73d7c3863b8e18a23906383b23dc3b73db8023
Kevin Lin <kevinlin47@hotmail.com>
2018-04-03 00:09:11 -0400

    Monkey Sort

--- MonkeySort.py
+++ MonkeySort.py
@@ -1,7 +1,7 @@
 import random
 
 def isSorted(data):
-    for i in range(0,len(data-1)):
+    for i in range(0,len(data)-1):
         if data[i]>data[i+1]:
             return False
     return True
@@ -9,4 +9,9 @@ def isSorted(data):
 def monkeysort(data):
     while not isSorted(data):
         random.shuffle(data)
+
+data=[1,19,3,27,1995,5]
+
+monkeysort(data)
+print(data)
     
